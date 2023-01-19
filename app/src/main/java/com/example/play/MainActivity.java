3b4e5c7bb17f2f2be7ce2c761707b57780b68b2d
yonataniluz12 <95865008+yonataniluz12@users.noreply.github.com>
2023-01-19 18:35:10 +0200

    sherrrr

--- app/src/main/java/com/example/play/MainActivity.java
+++ app/src/main/java/com/example/play/MainActivity.java
@@ -4,57 +4,42 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3;
-    int countRed ;
-    int countBlue ;
-
+    private int count1, count2;
+    Intent si;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
-        countRed = 0;
-        countBlue = 0;
-        btn2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                countBlue++;
-            }
+        count1 = 0;
+        count2 = 0;
+        si = new Intent(this, results_game.class);
+        btn1.setBackgroundColor(Color.RED);
+        btn2.setBackgroundColor(Color.BLUE);
+        btn1.setOnClickListener(view -> count1++);
+
+        btn3.setOnClickListener(view -> {
+            si.putExtra("a", count1);
+            si.putExtra("aa", count2);
+            count1 = 0;
+            count2 = 0;
+            startActivity(si);
         });
-        btn1.setOnLongClickListener(this);
-    }
 
-
-    @Override
-    public boolean onLongClick(View view) {
-        countRed += 2;
-        return false;
-    }
-
-
-    public void go(View view) {
-        Intent si = new Intent(this,results_game.class);
-        if(countBlue>countRed){
-            si.putExtra("a","blue");}
-        else{
-            si.putExtra("a","red");
-        }
-        si.putExtra("aa",countBlue);
-        si.putExtra("aaa",countRed);
-        startActivityForResult(si,1);
-    }
-    protected void onActivityResult(int source, int good, @Nullable Intent data_back) {
-        super.onActivityResult(source, good, data_back);
-        if(data_back != null){
-            countRed = data_back.getIntExtra("n",-999);
-            countRed = data_back.getIntExtra("n",-999);
-        }
+        btn2.setOnLongClickListener(view -> {
+            count2 += 2;
+            return false;
+        });
     }
 }
--- app/src/main/java/com/example/play/results_game.java
+++ app/src/main/java/com/example/play/results_game.java
@@ -2,34 +2,40 @@ package com.example.play;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class results_game extends AppCompatActivity {
-    Button btn;
-    TextView tV,tV2,tV3;
-    Intent gi = getIntent();
-    String st = gi.getStringExtra("a");
-    int red  = gi.getIntExtra("aaa",-999);
-    int blue = gi.getIntExtra("aa",-999);
+    Button btn4;
+    TextView tV1,tV2,tV3;
+
+    @SuppressLint({"SetTextI18n", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_game);
-        tV = findViewById(R.id.tV);
-        tV2 = findViewById(R.id.tV2);
-        tV3 = findViewById(R.id.tV3);
-        tV2.setText(red);
-        tV3.setText(blue);
-        tV.setText(st);
-    }
 
-    public void goo(View view) {
-        gi.putExtra("n",0);
-        setResult(RESULT_OK,gi);
-        finish();
+        tV1=findViewById(R.id.tV1);
+        tV2=findViewById(R.id.tV2);
+        tV3=findViewById(R.id.tV3);
+        btn4=findViewById(R.id.btn4);
+        Intent gi=getIntent();
+        int count1= gi.getIntExtra("a", -999);
+        int count2= gi.getIntExtra("aa", -999);
+        tV2.setText("Player1: "+count1);
+        tV3.setText("Player2: "+count2);
+        if (count1>count2){
+            tV1.setText("PLAYER RED!");
+        }
+        else if (count1<count2){
+            tV1.setText("PLAYER BLUE!");
+        }
+        else{
+            tV1.setText("TEKO");
+        }
+        btn4.setOnClickListener(view -> finish());
     }
 }
